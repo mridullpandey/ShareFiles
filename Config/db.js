fa93036f28ab86470e1aa5b2a9608b728c40ff0e
mridullpandey <mridulpandey27@gmail.com>
2022-07-24 20:45:57 +0530

    initial

--- Config/db.js
+++ Config/db.js
@@ -4,16 +4,16 @@ function connectDB() {
   // Database connection 🥳
   mongoose.connect(process.env.MONGO_CONNECTION_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: true,
+    // useFindAndModify: true,
   });
   const connection = mongoose.connection;
   connection
     .once("open", () => {
       console.log("Database connected 🥳🥳🥳🥳");
     })
-    .catch((err) => {
+    .on("error", () => {
       console.log("Connection failed ☹️☹️☹️☹️");
     });
 }
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require("express");
-const connectDB = require("./Config/db");
 require("dotenv").config();
-connectDB();
-const app = express();
 
+const express = require("express");
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}.`);
-});
+const path = require("path");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+
+// Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(","),
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+};
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+
+const connectDB = require("./Config/db");
+connectDB();
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+// Routes
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
